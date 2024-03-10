0561aa3a2e6f1a6267133fee66618c06796d1a3c
etoto06 <160569827+etoto06@users.noreply.github.com>
2024-03-10 23:57:25 +0900

    Update 10871.py

--- baekjoon/10871.py
+++ baekjoon/10871.py
@@ -14,4 +14,17 @@ for B in A:
         print(B)
     
    
-        
+        N , X = input().split(" ")
+X = int(X)      #정수형으로 바꿔줘야한다 
+A = input().split()
+
+# for A in range(N):
+#     num = input()
+#     A.append(num)
+
+B = []
+
+for B in A:
+    B = int(B)
+    if X > B:
+        print(B)
